f7d391eddab9e61e72b3b221fe6e2b1c463551dc
Michele Biondi <michelebiondi01@gmail.com>
2020-03-30 16:44:44 +0200

    Add connection the local database

--- backend/tests/integration/parkingSpot.test.js
+++ backend/tests/integration/parkingSpot.test.js
@@ -3,6 +3,21 @@ const mongoose = require('mongoose');
 const dbHandler = require('./utils/dbHandler');
 const parkingSpotModel = require('./../../models/parkingSpot');
 
+/**
+ * Connect to a new in-memory database before running any tests.
+ */
+beforeAll(async () => await dbHandler.connect());
+
+/**
+ * Clear all test data after every test.
+ */
+afterEach(async () => await dbHandler.clearDatabase());
+
+/**
+ * Remove and close the db and server.
+ */
+afterAll(async () => await dbHandler.closeDatabase());
+
 const exampleValidParkingSpot = {
     latitude: 45.464203,
     longitude: 9.189982,
