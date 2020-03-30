57b1215ba5c76e2adbf19e66232dd408321c3233
corradopetrelli <corrado.petrelli@studenti.unicam.it>
2020-03-30 14:39:25 +0200

    Added Hello World expressjs

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("hello world!");
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', function(req, res) {
+    res.send('Hello world');
+});
+
+app.listen(PORT, function() {
+    console.log('Server is running on port:', PORT);
+});
