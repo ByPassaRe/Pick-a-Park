6de678df6f440eb36ef65f02bbef2faf0f72c9a7
corradopetrelli <corrado.petrelli@studenti.unicam.it>
2020-03-30 15:37:13 +0200

    Integrate mongoose and a test db to backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const db = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -11,6 +12,22 @@ app.get('/', function(req, res) {
     res.send('Hello world');
 });
 
+
+//Connection to Database
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+//Server listening
 app.listen(PORT, function() {
     console.log('Server is running on port:', PORT);
 });
