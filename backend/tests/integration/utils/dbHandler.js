d6bdf61bcd21e08afe10635828c6639beefeace1
Michele Biondi <michelebiondi01@gmail.com>
2020-03-30 16:25:26 +0200

    Add simple tests for parkingspot

---
+++ backend/tests/integration/utils/dbHandler.js
@@ -0,0 +1,34 @@
+/* Adapted from https://dev.to/paulasantamaria/testing-node-js-mongoose-with-an-in-memory-database-32np */
+
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const mongod = new MongoMemoryServer();
+
+module.exports.connect = async () => {
+    const uri = await mongod.getConnectionString();
+
+    const mongooseOpts = {
+        useNewUrlParser: true,
+        autoReconnect: true,
+        reconnectTries: Number.MAX_VALUE,
+        reconnectInterval: 1000
+    };
+
+    await mongoose.connect(uri, mongooseOpts);
+}
+
+module.exports.closeDatabase = async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongod.stop();
+}
+
+module.exports.clearDatabase = async () => {
+    const collections = mongoose.connection.collections;
+
+    for (const key in collections) {
+        const collection = collections[key];
+        await collection.deleteMany();
+    }
+}
