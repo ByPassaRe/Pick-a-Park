ab3f85e7c67ced1a73ff8e453d062790e1b4ccb7
corradopetrelli <corrado.petrelli@studenti.unicam.it>
2020-03-31 20:09:34 +0200

    Complete User creation form

--- frontend/src/pages/UserCreationForm.js
+++ frontend/src/pages/UserCreationForm.js
@@ -1,26 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function UserCreationForm() {
+  const [user, setUser] = useState({});
+  // eslint-disable-next-line
+  const emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+  // eslint-disable-next-line
+  const usernameRegex = /^[a-z0-9_-]{3,30}$/;
+
+  //Check methods
+  const isValidUsername = user => user.username.match(usernameRegex);
+  const isValidEmail = user => user.email.match(emailRegex);
+  const isConfirmEquals = user => user.password === user.confirmPassword
+  const isValidPassword = user => user.password.length >= 8
+
+  
+  const handleCorrectData = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/users', { 
+        username: user.username,
+        email: user.email,
+        password: user.password
+      });
+      response.status === 200 ? alert('User Created succesfully') : alert(response);
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const handleBadData = (err) => {
+    alert(err);
+  }
+
+  const handleSubmit = () => {
+    if(!isValidUsername(user))
+      handleBadData("Username not valid");
+    else if(!isValidPassword(user))
+      handleBadData("Password must have at least 8 characters");
+    else if(!isConfirmEquals(user))
+      handleBadData("Password and Confirm Password are different");
+    else if(!isValidEmail(user))
+      handleBadData("Email not valid");
+    else
+    handleCorrectData();
+  }
+
+
 
   return (
     <div>
       Username:
-      <input type="text"  name="username"/>
+      <input type="text"  name="username" onChange={(e) => setUser({...user, username: e.target.value})}/>
       <br />
 
       Password:
-      <input type="password" name="password"/>
+      <input type="password" name="password" onChange={(e) => setUser({...user, password: e.target.value})}/>
       <br />
 
       Confirm Password:
-      <input type="password" name="password"/>
+      <input type="password" name="password" onChange={(e) => setUser({...user, confirmPassword: e.target.value})}/>
       <br />
 
       Email:
-      <input type="text" name="email"/>
+      <input type="text" name="email" onChange={(e) => setUser({...user, email: e.target.value})}/>
       <br />
 
-      <button>Sign In</button>
+      <button onClick ={handleSubmit}>Sign In</button>
     </div>
   );
 }
