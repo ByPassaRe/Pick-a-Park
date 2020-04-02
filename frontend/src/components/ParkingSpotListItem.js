caa5987ad9baa55f89508e58690d0a1dc2ae9c84
Michele Biondi <michelebiondi01@gmail.com>
2020-04-02 16:17:52 +0200

    Created Parking List

--- frontend/src/components/ParkingSpotListItem.js
+++ frontend/src/components/ParkingSpotListItem.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const OutsetDiv = styled.div`
+    border-style: outset;
+    margin: 20px;
+`;
 
 const ParkingSpotListItem = (props) => {
     return (
-        <>
+        <OutsetDiv>
             <p>Id: {props.parkingSpot._id}</p>
             <p>Latitude: {props.parkingSpot.location.latitude}</p>
             <p>Longitude: {props.parkingSpot.location.longitude}</p>
-        </>
+        </OutsetDiv>
     );
 }
 
