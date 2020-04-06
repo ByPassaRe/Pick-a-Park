d1d297a23b967cc0860ccdb1c15972a0f479a125
Margherita <Renieri>
2020-04-06 16:18:15 +0200

    Add user Geo-Location as Origin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,9 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css' // Updating node module will keep css up to date.
 import React, { Component } from 'react';
 import ReactMapboxGl, {Layer, Feature} from "react-mapbox-gl";
-import mapboxgl, {GeolocateControl, Directions} from 'mapbox-gl';
-import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import mapboxgl, {GeolocateControl} from 'mapbox-gl';
+//import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
-
-
 
 mapboxgl.accessToken= "pk.eyJ1IjoibWFyZ2hlcml0YXJlbmllcmk5NiIsImEiOiJjazN4bzl0MXowZDd6M2xwNm5xbmZrZ2oxIn0.HAkjmhv5QblYNTnR_ZKiQg";
 const Map = ReactMapboxGl({
@@ -17,18 +15,26 @@ const Map = ReactMapboxGl({
   interactive: true,
   bearingSnap: 3
 });
-const flyToOptions ={
-  center: [0,0],
-  zoom:9,
-  speed:0.8,
-  curve:1
-}
-
 const mapStyle = {
   flex: 1,
   height: '90vh',
   width: '80vw'
 };
+var directions = new MapboxDirections({accessToken: mapboxgl.accessToken,
+  unit: 'metric',
+  profile: 'mapbox/driving',
+  interactive: true,
+	controls: {
+    inputs: true,
+    instructions:false,
+		profileSwitcher: false
+  },
+  geocoder: {
+		language: 'it'
+  },
+  placeholderOrigin: 'Inserisci origine',
+  placeholderDestination: 'Inserisci destinazione'
+  });
 const onMapLoad = (map) =>{
   map.addControl(
     new GeolocateControl({
@@ -36,27 +42,35 @@ const onMapLoad = (map) =>{
         enableHighAccurancy: true
       },
       trackUserLocation: true
-    }),
+        }),
   'top-left');
   map.addControl(new mapboxgl.FullscreenControl(),'top-left');
   map.addControl(new mapboxgl.NavigationControl(),'top-left');
-  map.addControl(new MapboxGeocoder({accessToken:mapboxgl.accessToken, mapboxgl: mapboxgl }), 'top-right');
-  map.addControl(new MapboxDirections({accessToken: mapboxgl.accessToken, unit:'metric', profile:'mapbox/driving' }), 'top-right');
-
+ // map.addControl(new MapboxGeocoder({accessToken:mapboxgl.accessToken, mapboxgl: mapboxgl }), 'top-right');
+  map.addControl(directions, 'top-right');
  };
+ if ("geolocation" in navigator) { 
+  navigator.geolocation.getCurrentPosition(position => { 
+  var lon = position.coords.longitude;
+  var lat = position.coords.latitude;
+      var map = new mapboxgl.Map({
+        container: 'root',
+       // initial position in [lon, lat] format
+        center: [lon, lat]
+      });
+      directions.setOrigin([lon,lat]);
+  }); 
+} else { /* geolocation IS NOT available, handle it */ }
 class MapPage extends Component {  
-  
   render() {
     return (
   <Map 
   style="mapbox://styles/mapbox/streets-v11"
   containerStyle={mapStyle}
-  flyToOption={flyToOptions} 
   onStyleLoad={onMapLoad}
   >
   </Map>
-
-
   )}
 }
   export default MapPage; 
+
