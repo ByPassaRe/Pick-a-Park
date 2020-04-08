3ec2ab2ea8531ffe4fa2470d2ae6d41a036be0dd
Michele Biondi <michelebiondi01@gmail.com>
2020-04-08 17:26:56 +0200

    Add old pages + not completed set price page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import UserCreationForm from './pages/UserCreationForm';
 import LoginForm from './pages/LoginForm';
+import ParkingSpotListView from './pages/ParkingSpotListView';
+import ParkingSpotsSetPriceView from './pages/ParkingSpotsSetPriceView';
+import ParkingSpotCreationForm from './pages/ParkingSpotCreationForm';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
@@ -30,11 +33,23 @@ function App() {
             <li>
               <Link to="/users">Create User</Link>
             </li>
+            <li>
+              <Link to="/parkingSpotCreation">Parking Spot Creation</Link>
+            </li>
+            <li>
+              <Link to="/parkingSpotListView">Parking Spots List</Link>
+            </li>
+            <li>
+              <Link to="/parkingSpotsSetPriceView">Set parking spot prices</Link>
+            </li>
           </ul>
         </nav>
 
         <Route path="/auth" component={LoginForm} />
         <Route path="/users" component={UserCreationForm} />
+        <Route path="/parkingSpotCreation" component={ParkingSpotCreationForm} />
+        <Route path="/parkingSpotListView" component={ParkingSpotListView} />
+        <Route path="/parkingSpotsSetPriceView" component={ParkingSpotsSetPriceView} />
       </div>
     </Router>
   );
---
+++ frontend/src/pages/ParkingSpotsSetPriceView.js
@@ -0,0 +1,38 @@
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import ParkingSpotListItem from './../components/ParkingSpotListItem';
+
+function ParkingSpotListView() {
+    const [statusMessage, setStatusMessage] = useState(null);
+    const [parkingSpots, setParkingSpots] = useState(null);
+
+    useEffect(() => {
+        (async function fetchParkingSpots () {
+            setStatusMessage('Loading...');
+            
+            try {
+                const response = await axios.get('http://localhost:5000/parkingSpots');
+                setParkingSpots(response.data.parkingSpots);
+            } catch (err) {
+                setStatusMessage('Error retrieving data.');
+            };
+            
+        })();
+
+    }, []);
+
+    const renderParkingSpot = (parkingSpot) => <ParkingSpotListItem key={parkingSpot._id} parkingSpot={parkingSpot}/>
+
+    return (
+        <>
+        <h2>Parking Spots prices:</h2>
+        { parkingSpots ? 
+            parkingSpots.map( renderParkingSpot ) : 
+            <p>{statusMessage}</p> }
+        </>
+    );
+}
+
+export default ParkingSpotListView;
