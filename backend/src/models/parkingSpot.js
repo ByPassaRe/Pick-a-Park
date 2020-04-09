4e64a4b6bda2191aee3c668b1bd7b844b0275994
Michele Biondi <michelebiondi01@gmail.com>
2020-04-09 09:44:17 +0200

    Created price field on parking spot model

--- backend/src/models/parkingSpot.js
+++ backend/src/models/parkingSpot.js
@@ -5,6 +5,7 @@ const ParkingSpotSchema = new mongoose.Schema({
         latitude: {type:Number, min: -90, max: 90, required: true},
         longitude: {type:Number, min: -180, max: 180, required: true},
     },
+    price: {type: Number, default: 0, min: 0},
     available: {type: Boolean, default: false}
 });
 
