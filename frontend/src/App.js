c9ccf49de2eabbdfcb5e8b80971114eaf6a973b5
Margherita <Renieri>
2020-04-09 15:53:30 +0200

    Add [long, lat] print of origin and destination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import "mapbox-gl/dist/mapbox-gl.css";
-import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css' // Updating node module will keep css up to date.
-import React, { Component } from 'react';
-import ReactMapboxGl, {Layer, Feature} from "react-mapbox-gl";
-import mapboxgl, {GeolocateControl} from 'mapbox-gl';
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+import React, { Component} from 'react';
+import ReactMapboxGl from "react-mapbox-gl";
+import mapboxgl, { GeolocateControl } from 'mapbox-gl';
 //import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
 
-mapboxgl.accessToken= "pk.eyJ1IjoibWFyZ2hlcml0YXJlbmllcmk5NiIsImEiOiJjazN4bzl0MXowZDd6M2xwNm5xbmZrZ2oxIn0.HAkjmhv5QblYNTnR_ZKiQg";
+mapboxgl.accessToken = "pk.eyJ1IjoibWFyZ2hlcml0YXJlbmllcmk5NiIsImEiOiJjazN4bzl0MXowZDd6M2xwNm5xbmZrZ2oxIn0.HAkjmhv5QblYNTnR_ZKiQg";
 const Map = ReactMapboxGl({
-  container:'map',
+  container: 'map',
   accessToken: "pk.eyJ1IjoibWFyZ2hlcml0YXJlbmllcmk5NiIsImEiOiJjazN4bzl0MXowZDd6M2xwNm5xbmZrZ2oxIn0.HAkjmhv5QblYNTnR_ZKiQg",
-  minZoom:4,
-  maxZoom:15,
+  minZoom: 4,
+  maxZoom: 15,
   interactive: true,
   bearingSnap: 3
 });
@@ -20,57 +20,81 @@ const mapStyle = {
   height: '90vh',
   width: '80vw'
 };
-var directions = new MapboxDirections({accessToken: mapboxgl.accessToken,
+var directions = new MapboxDirections({
+  accessToken: mapboxgl.accessToken,
   unit: 'metric',
   profile: 'mapbox/driving',
   interactive: true,
-	controls: {
+  controls: {
     inputs: true,
-    instructions:false,
-		profileSwitcher: false
+    instructions: false,
+    profileSwitcher: false
   },
   geocoder: {
-		language: 'it'
+    language: 'it'
   },
-  placeholderOrigin: 'Inserisci origine',
+  placeholderOrigin: 'La tua posizione',
   placeholderDestination: 'Inserisci destinazione'
-  });
-const onMapLoad = (map) =>{
+});
+/**
+var geocoder = new MapboxGeocoder(
+  {accessToken:mapboxgl.accessToken, mapboxgl: mapboxgl }
+);
+*/
+
+const onMapLoad = (map) => {
   map.addControl(
     new GeolocateControl({
-      positionOptions:{
+      positionOptions: {
         enableHighAccurancy: true
       },
       trackUserLocation: true
-        }),
-  'top-left');
-  map.addControl(new mapboxgl.FullscreenControl(),'top-left');
-  map.addControl(new mapboxgl.NavigationControl(),'top-left');
- // map.addControl(new MapboxGeocoder({accessToken:mapboxgl.accessToken, mapboxgl: mapboxgl }), 'top-right');
+    }),
+    'top-left');
+  map.addControl(new mapboxgl.FullscreenControl(), 'top-left');
+  map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+  //map.addControl(geocoder, 'top-right');
   map.addControl(directions, 'top-right');
- };
- if ("geolocation" in navigator) { 
-  navigator.geolocation.getCurrentPosition(position => { 
-  var lon = position.coords.longitude;
-  var lat = position.coords.latitude;
-      var map = new mapboxgl.Map({
-        container: 'root',
-       // initial position in [lon, lat] format
-        center: [lon, lat]
+  //var lonDest= directions.getDestination().geometry.coordinates[0];
+  //var latDest= directions.getDestination().geometry.coordinates[1];
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(position => {
+      var lon = position.coords.longitude;
+      var lat = position.coords.latitude;
+      directions.setOrigin([lon, lat]);
+      document.getElementById('load').innerHTML =
+        'la mia posizione è:' +
+        JSON.stringify(directions.getOrigin().geometry.coordinates);
+      map.on('click', function () {
+        if (directions.getDestination()) {
+          document.getElementById('info').innerHTML =
+            'la posizione iniziale è:' +
+            JSON.stringify(directions.getOrigin().geometry.coordinates) + '<br />' +
+            'la destinazione inserita ha le seguenti coordinate:' +
+            JSON.stringify(directions.getDestination().geometry.coordinates);
+        }
+
       });
-      directions.setOrigin([lon,lat]);
-  }); 
-} else { /* geolocation IS NOT available, handle it */ }
-class MapPage extends Component {  
+
+    });
+
+  } else {
+    alert("Impossibile calcolare la tua posizione");
+  }
+};
+
+
+class MapPage extends Component {
   render() {
     return (
-  <Map 
-  style="mapbox://styles/mapbox/streets-v11"
-  containerStyle={mapStyle}
-  onStyleLoad={onMapLoad}
-  >
-  </Map>
-  )}
+      <Map
+        style="mapbox://styles/mapbox/streets-v11"
+        containerStyle={mapStyle}
+        onStyleLoad={onMapLoad}
+      >
+      </Map>
+    )
+  }
 }
-  export default MapPage; 
+export default MapPage;
 
