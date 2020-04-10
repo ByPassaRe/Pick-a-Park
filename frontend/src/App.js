3f076e315daa0d6b3f10ecb61967fbd1ddeb5063
corradopetrelli <corrado.petrelli@studenti.unicam.it>
2020-04-10 11:22:11 +0200

    Update App.js for testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import LogoutButton from './pages/LogoutButton';
 import LoginForm from './pages/LoginForm';
+import UserCreationForm from './pages/UserCreationForm';
 
 function App() {
   return (
@@ -20,20 +21,26 @@ function App() {
     */
     //<ParkingSpotCreationForm/>
     
+    
     <Router>
       <div>
-
-      <LogoutButton />
         <nav>
           <ul>
             <li>
               <Link to="/auth">Login</Link>
             </li>
-            
+            <li>
+              <Link to="/users">Create User</Link>
+            </li>
           </ul>
         </nav>
 
         <Route path="/auth" component={LoginForm} />
+        <Route path="/users" component={UserCreationForm} />
+        <p>
+          ----
+        </p>
+        <Route component={LogoutButton} />
       </div>
     </Router>
   );
--- frontend/src/pages/LogoutButton.js
+++ frontend/src/pages/LogoutButton.js
@@ -3,8 +3,10 @@ import React from 'react';
 function LogoutButton() {
   
   const handleSubmit = () => {
-    if(localStorage.getItem('jwt'))
+    if(localStorage.getItem('jwt')){
       localStorage.removeItem('jwt');
+      alert("You are logged off!");
+    }
     else
       alert("You are not logged!");
   }
