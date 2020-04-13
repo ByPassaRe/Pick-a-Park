46eef8fe8b1ff0e83174d6df396d1bdff033b99a
corradopetrelli <corrado.petrelli@studenti.unicam.it>
2020-04-13 14:15:30 +0200

    Introduced api (does not work) to change password

--- frontend/src/pages/ChangePasswordForm.js
+++ frontend/src/pages/ChangePasswordForm.js
@@ -3,11 +3,35 @@ import axios from 'axios';
 
 function ChangePasswordForm() {
   const [data, setData] = useState({});
+ 
+ 
+  
+  const handleCorrectData = async () => {
+    try {
+      //I try to change the password
+      //the checking of the actual password is moved to the server
+      const response = await axios.post('http://localhost:5000/changePassword', { 
+        actualPassword: data.actualPassword,
+        newPassword: data.newPassword,
+        confirmNewPassword: data.confirmNewPassword
+      });
+      response.status === 200 ? alert('Password changed!') : alert("Wrong actual password!");
+    } catch (err) {
+      alert(err);
+    }
+  };
 
+  const handleBadData = (err) => {
+    alert(err);
+  }
 
   const handleSubmit = () => {
-    
-    console.log(data);
+       if(data.newPassword !== data.confirmNewPassword)
+      handleBadData("The two passwords are different");
+    else if(data.newPassword.length < 8)
+      handleBadData("The new password must have at least 8 characters");
+    else
+      handleCorrectData();
   }
 
 
