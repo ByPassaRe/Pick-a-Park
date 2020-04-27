6f71590c545f3b5dff28558580932d8b6e4dd511
corradopetrelli <corrado.petrelli@studenti.unicam.it>
2020-04-27 10:54:32 +0200

    Introduced depositing of money - only frontend

--- frontend/src/pages/WalletPage.js
+++ frontend/src/pages/WalletPage.js
@@ -23,11 +23,57 @@ function WallerPage() {
 
     }, []);
 
+    const AmountAdder = () => {
+        const [amountToAdd, setAmountToAdd] = useState(0);
+
+        const handleSend = async () => {
+            try {
+                //CHIAMATA API Per cambiare il prezzo sul db
+                /*await axios.patch(`http://localhost:5000/parkingSpots/${props.parkingSpot._id}`, {
+                    price: newPrice
+                });*/
+            } catch (err) {
+                alert(err);
+            }
+
+            alert("New amount set succesfully");
+            /*
+                Why +num1 + +num2?
+                Because num1,num2 are strings and I need to convert them to number
+            */
+            setWalletAmount(+walletAmount + +amountToAdd);
+
+        }
+
+        const handleChange = (e) => {
+            if(e.target.value < 0) {
+                alert("You can't delete cash!")
+                setAmountToAdd(0);
+                return;
+            }
+            setAmountToAdd(e.target.value);
+        }
+
+        return (
+            <>
+                Set amount to add:
+                <input type="number" min="0" step="any" value={amountToAdd} onChange={handleChange}/> 
+                <button onClick={handleSend}>Update</button>
+            </>
+        )
+}
+
   return (
         <>
         <h2>Wallet</h2>
         {
-            walletAmount? <p>You have {walletAmount} €</p> : <p>{statusMessage}</p>
+            walletAmount? 
+                <div>
+                    <p>You have {walletAmount} €</p> 
+                    <AmountAdder/>
+                </div>
+                :
+                <p>{statusMessage}</p>
         }
         
         </>
