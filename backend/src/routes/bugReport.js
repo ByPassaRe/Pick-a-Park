2ff3176f5e45d46c2125b8e317909a9fa34a6f4b
Michele Biondi <michelebiondi01@gmail.com>
2020-04-29 13:34:54 +0200

    Add authorization for get route

--- backend/src/routes/bugReport.js
+++ backend/src/routes/bugReport.js
@@ -1,7 +1,9 @@
 const bugReports = require('./../controllers/bugReport');
 const router = require("express").Router();
+const authorize = require("../util/authorize");
+const role = require("../util/role");
 
 router.post('/', bugReports.create);
-router.get('/', bugReports.get);
+router.get('/', authorize([role.PARKING_COMPANY]), bugReports.get);
 
 module.exports = router;
