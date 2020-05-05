2b4cebf266f9822fdf09e220ca6f87f07121c77a
corradopetrelli <corrado.petrelli@studenti.unicam.it>
2020-05-05 21:26:58 +0200

    authorization - bug report route

--- backend/src/routes/bugReport.js
+++ backend/src/routes/bugReport.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const authorize = require("../util/authorize");
 const role = require("../util/role");
 
-router.post('/', bugReports.create);
+router.post('/',authorize([role.DRIVER, role.MUNICIPALITY_EMPLOYEE, role.MUNICIPALITY_POLICE]), bugReports.create);
 router.get('/', authorize([role.PARKING_COMPANY]), bugReports.get);
 
 module.exports = router;
