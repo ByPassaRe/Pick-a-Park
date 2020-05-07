99c2babb5acd2b85ce991ac63deffa896e5af4af
corradopetrelli <corrado.petrelli@studenti.unicam.it>
2020-05-07 18:14:38 +0200

    Documentation api user.js done

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -3,57 +3,172 @@ const users = require("../controllers/user.js");
 const router = require("express").Router();
 const authorize = require("../util/authorize");
 const role = require("../util/role");
+ /**
+  * @swagger
+  * tags:
+  *   name: Users
+  */
+ /**
+  * @swagger
+  *     components:
+  *         schemas:
+  *             User:
+  *                 description: A JSON object containing User information
+  *                 properties:
+  *                     email:
+  *                         type: string
+  *                     username:
+  *                         type: string
+  *                     password:
+  *                         type: string
+  *                     role:
+  *                         type: string
+  *                         enum:
+  *                             - DRIVER
+  *                             - MUNICIPALITY_EMPLOYEE
+  *                             - MUNICIPALITY_POLICE
+  *                             - PARKING_COMPANY    
+  *         securitySchemes:
+  *             bearerAuth:
+  *                 type: apiKey
+  *                 in: header
+  *                 name: Authorization
+  */
 
-/**
- * Create a new user
- * @route POST /users/
- * @group User - Operations about user
- * @param {string} email.query.required - user's email - eg: valeria.vinci@studenti.unicam.it
- * @param {string} username.query.required - user's username - eg: valeria
- * @param {string} password.query.required - user's password.
- * @param {enum} role.query - user's role - eg: DRIVER,MUNICIPALITY_EMPLOYEE,MUNICIPALITY_POLICE,PARKING_COMPANY
- * @returns {object} 200 - An array of user info 
- * @returns {Error}  400 - Unexpected error
- */
+ /**
+  * @swagger
+  * /users/:
+  *     post:
+  *         description: Create an user
+  *         tags: [Users]
+  *         requestBody:
+  *             required: true
+  *             content:
+  *                 application/json:
+  *                     schema:
+  *                         $ref: '#/components/schemas/User'
+  *                     examples:
+  *                         Massimo DRIVER:
+  *                             value:
+  *                                 email: massimo@test.it
+  *                                 username: massimo
+  *                                 password: massimomassimo
+  *                                 role: DRIVER
+  *                         Ludovico PARKING_COMPANY:
+  *                             value:
+  *                                 email: ludovico@test.it
+  *                                 username: ludovico
+  *                                 password: ludovicoludovico
+  *                                 role: PARKING_COMPANY
+  *                         Carlo MUNICIPALITY_EMPLOYEE:
+  *                             value:
+  *                                 email: carlo@test.it
+  *                                 username: carlo
+  *                                 password: carlocarlo
+  *                                 role: MUNICIPALITY_EMPLOYEE
+  *                         Gennaro MUNICIPALITY_POLICE:
+  *                             value:
+  *                                 email: gennaro@test.it
+  *                                 username: gennaro
+  *                                 password: gennarogennaro
+  *                                 role: MUNICIPALITY_POLICE
+  *         responses:
+  *             "200":
+  *                 description: An array of user info (password hashed)
+  *             "400":
+  *                 description: Unexpected error
+  */
 router.post("/",
     users.create);
     
+
 /**
- * Allow to an user to change password
- * @route POST /users/change-password
- * @group User - Operations about user
- * @param {string} actualPassword.query.required - user's old password.
- * @param {string} newPassword.query.required - user's new password.
- * @returns {object} 200 - A successful message 
- * @returns {object} 401 - Wrong actual password or New password does not match with requirements
- * @returns {object} 404 - User not found
- * @security JWT
- */
+  * @swagger
+  * /users/change-password:
+  *     post:
+  *         security:
+  *             - bearerAuth: []
+  *         description: Allow to an user to change password - Authorization required
+  *         tags: [Users]
+  *         requestBody:
+  *             required: true
+  *             content:
+  *                 application/json:
+  *                     schema:
+  *                         type: object
+  *                         properties:
+  *                             actualPassword:
+  *                                 type: string
+  *                             newPassword:
+  *                                 type: string
+  *         responses:
+  *             "200":
+  *                 description: A successful message
+  *             "401":
+  *                 description: Wrong actual password or New password does not match with requirements
+  *             "404":
+  *                 description: User not found
+  */
 router.post("/change-password",
     authorize(),
     users.changePassword);
 
 /**
- * Allow to get user's balance
- * @route GET /users/balance
- * @group User - Operations about user
- * @returns {object} 200 - A successful message 
- * @returns {object} 400 - Invalid JWT token
- * @security JWT
- */
+  * @swagger
+  * /users/balance:
+  *     get:
+  *         security:
+  *             - bearerAuth: []
+  *         description: Allow to get user's balance - Only Drivers are authorized
+  *         tags: [Users]
+  *         responses:
+  *             "200":
+  *                 description: A successful message with balance amount
+  *                 content:
+  *                     application/json:
+  *                         schema:
+  *                             type: object
+  *                             properties:
+  *                                 balance:
+  *                                     type: number
+  *             "400":
+  *                 description: Invalid JWT token
+  */
 router.get("/balance",
     authorize([role.DRIVER]),
     users.getBalance);
 
+
 /**
- * Allow to charge user's balance
- * @route PATCH /users/chargeBalance
- * @group User - Operations about user
- * @param {string} amount.query.required - amount to charge.
- * @returns {object} 200 - Return new balance 
- * @returns {object} 400 - Invalid JWT token
- * @security JWT
- */    
+* @swagger
+* /users/chargeBalance:
+*     patch:
+*         security:
+*             - bearerAuth: []
+*         description: Allow to charge user's balance - Only Drivers are authorized
+*         tags: [Users]
+*         requestBody:
+*             required: true
+*             content:
+*                 application/json:
+*                     schema:
+*                         type: object
+*                         properties:
+*                             amount:
+*                                 type: number
+*         responses:
+*             "200":
+*                 description: A successful message with updated balance
+*                 content:
+*                     application/json:
+*                         schema:
+*                             type: object
+*                             properties:
+*                                 balance:
+*                                     type: number
+*             "400":
+*                 description: Invalid JWT token
+*/   
 router.patch("/chargeBalance",
     authorize([role.DRIVER]),
     users.chargeBalance);
