bf6cb903b9220f2004c68f9dba00e3107901201c
corradopetrelli <corrado.petrelli@studenti.unicam.it>
2020-05-07 15:18:16 +0200

    Changed middleware swagger

--- backend/src/config/swaggerOptions.js
+++ backend/src/config/swaggerOptions.js
@@ -1,34 +1,25 @@
 module.exports =  {
-    swaggerDefinition: {
-        info: {
-            title: 'Pick-A-Park API',
-            version: '1.0.0',
-            description:
-              "Project of a smart parking app for SPM exam at University of Camerino",
-            license: {
-              name: "License MIT",
-              url: "https://choosealicense.com/licenses/mit/"
-            },
-            contact: {
-              name: "Corrado Petrelli",
-              email: "corrado.petrelli@studenti.unicam.it"
-            }
-        },
-        host: 'localhost:5000',
-        produces: [
-            "application/json",
-            "application/xml"
-        ],
-        schemes: ['http', 'https'],
-		securityDefinitions: {
-            JWT: {
-                type: 'apiKey',
-                in: 'header',
-                name: 'Authorization',
-                description: "",
-            }
-        }
-    },
-    basedir: require('path').resolve(__dirname, '..'), //app absolute path of parent
-    files: ['./routes/**/*.js'], //Path to the API handle folder
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: 'Pick-A-Park API',
+      version: '1.0.0',
+      description:
+        "Project of a smart parking app for SPM exam at University of Camerino a.y. 2019-2020",
+      license: {
+        name: "License MIT",
+        url: "https://choosealicense.com/licenses/mit/"
+      },
+      contact: {
+        name: "- Pick-A-Park email",
+        email: "corrado.petrelli@studenti.unicam.it"
+      },
+      externalDocs: {
+        description: "Wiki",
+        url: "https://github.com/ByPassaRe/Pick-a-Park/wiki"
+      }
+    }
+  },
+  apis: ["../routes/**/*.js"]
 };
+
