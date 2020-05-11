30be5896832ee8abc0d5f26ee499ea61cfaaa975
Michele Biondi <michelebiondi01@gmail.com>
2020-05-11 16:04:39 +0200

    Fixes

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -5,11 +5,11 @@ import axios from 'axios';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
- 
+
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFyZ2hlcml0YXJlbmllcmk5NiIsImEiOiJjazN4bzl0MXowZDd6M2xwNm5xbmZrZ2oxIn0.HAkjmhv5QblYNTnR_ZKiQg';
 
 let eventFired = false;
-const findParkingSpot = async (lonDest,latDest) => {
+const findParkingSpot = async (lonDest, latDest) => {
 
   try {
     const result = await axios.get('http://localhost:5000/parkingSpots/nearest', {
@@ -28,57 +28,57 @@ const findParkingSpot = async (lonDest,latDest) => {
 }
 
 class Map extends React.Component {
-    
-    componentDidMount() {
 
-        const intiialCoords = [this.props.longitude, this.props.latitude];
+  componentDidMount() {
 
-        const map = new mapboxgl.Map({
-            container: this.mapContainer,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: intiialCoords,
-            zoom: 13
-        });
+    const intiialCoords = [this.props.longitude, this.props.latitude];
 
-        const directions = new MapboxDirections({
-            accessToken: mapboxgl.accessToken,
-            unit: 'metric',
-            profile: 'mapbox/driving',
-            controls: {
-              inputs: true,
-              instructions: false,
-              profileSwitcher: false,
-            },
-            placeholderOrigin: 'Loading your position....',
-        });
-      
-        map.addControl(directions, 'top-right');
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: intiialCoords,
+      zoom: 13
+    });
 
-        map.on('load', function () {
-          directions.setOrigin(intiialCoords);
-        });
+    const directions = new MapboxDirections({
+      accessToken: mapboxgl.accessToken,
+      unit: 'metric',
+      profile: 'mapbox/driving',
+      controls: {
+        inputs: true,
+        instructions: false,
+        profileSwitcher: false,
+      },
+      placeholderOrigin: 'Loading your position....',
+    });
 
-        directions.on('route', async () => {
-          console.log(eventFired);
-          if(eventFired === true){
-            eventFired = false;
-            return;
-          }
-          var lonDest= directions.getDestination().geometry.coordinates[0];
-          var latDest= directions.getDestination().geometry.coordinates[1];
-        
-          directions.setDestination(await findParkingSpot(lonDest, latDest));
-          eventFired = true;
-        });
-    }
+    map.addControl(directions, 'top-right');
 
-    render() {
-        return (
-          <div>
-              <div style={{height: '600px', width: '100%'}}ref={el => this.mapContainer = el}/>
-          </div>
-        )
-    }
+    map.on('load', function () {
+      directions.setOrigin(intiialCoords);
+    });
+
+    directions.on('route', async () => {
+      console.log(eventFired);
+      if (eventFired === true) {
+        eventFired = false;
+        return;
+      }
+      var lonDest = directions.getDestination().geometry.coordinates[0];
+      var latDest = directions.getDestination().geometry.coordinates[1];
+
+      directions.setDestination(await findParkingSpot(lonDest, latDest));
+      eventFired = true;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{ height: '600px', width: '100%' }} ref={el => this.mapContainer = el} />
+      </div>
+    )
+  }
 }
 
 export default Map;
