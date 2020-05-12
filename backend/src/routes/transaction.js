2d51503473f998d8a18658e82674a3eb2d8bf269
corradopetrelli <corrado.petrelli@studenti.unicam.it>
2020-05-12 09:13:41 +0200

    Added to transaction route swagger documentation

--- backend/src/routes/transaction.js
+++ backend/src/routes/transaction.js
@@ -3,6 +3,51 @@ const router = require("express").Router();
 const authorize = require("../util/authorize");
 const role = require("../util/role");
 
+
+   
+/**
+  * @swagger
+  * tags:
+  *   name: Transactions
+  */
+/**
+  * @swagger
+  *     components:
+  *         schemas:
+  *             Transaction:
+  *                 description: A JSON object containing Transaction information
+  *                 properties:
+  *                     amount:
+  *                         type: number
+  *                     userId:
+  *                         type: string   
+  *         securitySchemes:
+  *             bearerAuth:
+  *                 type: apiKey
+  *                 in: header
+  *                 name: Authorization
+  */
+
+ /**
+  * @swagger
+  * /transactions/:
+  *     get:
+  *         security:
+  *             - bearerAuth: []
+  *         description: Return all transactions - 
+  *         tags: [Transactions]
+  *         responses:
+  *             "200":
+  *                 description: An array of transactions - Only Parking Company are Authorized
+  *                 content:
+  *                     application/json:
+  *                         schema:
+  *                             type: array
+  *                             items:
+  *                                 $ref: '#/components/schemas/Transaction'
+  *             "400":
+  *                 description: Unexpected error
+  */
 router.get("/",
     authorize([role.PARKING_COMPANY]),
     transactions.read);
