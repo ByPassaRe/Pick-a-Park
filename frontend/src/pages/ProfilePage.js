5e4023e7b9533781f6693a11235640504a10d577
Margherita <margherita.renieri@studenti.unicam.it>
2020-05-23 13:08:59 +0200

    Add Sider to Driver Page

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { jwtDecode } from 'jwt-js-decode';
 import { Redirect } from 'react-router-dom';
 import MapPage from './MapPage';
@@ -11,33 +11,39 @@ import BugReportParkingCompanyView from './BugReportParkingCompanyView';
 import IssueCreationForm from './IssueCreationForm';
 import IssueListView from './IssueListView';
 import WalletPage from './WalletPage';
-import { Typography, Divider } from 'antd';
+import { Typography, Divider, Menu, Layout, Breadcrumb } from 'antd';
+import {FileOutlined,UserOutlined} from '@ant-design/icons';
 import '../App.css';
 
 
 function ProfilePage() {
+
+
+
     const { username, role } = jwtDecode(localStorage.jwt).payload;
-    
+
+
 
     let componentProfile = null;
+    const { SubMenu } = Menu;
+    const { Header, Content, Footer, Sider } = Layout;
+
 
     switch (role) {
         case "DRIVER":
             componentProfile =
-                <>  
+                <SubMenu key="sub1" icon={<FileOutlined />} title={<span>handle your profile</span>} >
+                    <Menu.Item key="3">Crea Bug Report</Menu.Item>
+                    <Menu.Item key="4">Setta il wallet</Menu.Item>
+                    <Menu.Item key="5">Seleziona Mappa</Menu.Item>
+                </SubMenu>
 
-                    <BugReportForm />
-                    <Divider/>
-                    <WalletPage />
-                    <Divider/>
-                    <MapPage />
-                </>
             break;
         case "PARKING_COMPANY":
             componentProfile =
                 <>
                     <BugReportParkingCompanyView />
-                    <Divider/>
+                    <Divider />
                     <ParkingSpotsSetPriceView />
                 </>
             break;
@@ -45,9 +51,9 @@ function ProfilePage() {
             componentProfile =
                 <>
                     <BugReportForm />
-                    <Divider/>
+                    <Divider />
                     <ParkingSpotCreationForm />
-                    <Divider/>
+                    <Divider />
                     <ParkingSpotMunicipalityView />
                 </>
             break;
@@ -55,7 +61,7 @@ function ProfilePage() {
             componentProfile =
                 <>
                     <IssueCreationForm />
-                    <Divider/>
+                    <Divider />
                     <IssueListView />
                 </>
             break;
@@ -68,16 +74,34 @@ function ProfilePage() {
     const { Title } = Typography;
 
 
+
+
     return (
-        <div style={{
-            position: 'absolute'
-        }}
-        >
-            <Title > Hi {username} you are a {role}</Title>
-            <ChangePasswordForm />
-            <Divider/>
-            {componentProfile}
-        </div>
+        <>
+            <Layout style={{ minHeight: '200vh' }}>
+                <Sider collapsible >
+                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                        <SubMenu key="sub1" icon={<UserOutlined />} title={<span>hi {username}</span>}>
+                            <Menu.Item key="1">Cambia Password</Menu.Item>
+                            <Menu.Item key="2">Logout</Menu.Item>
+                        </SubMenu>
+                        {componentProfile}
+                    </Menu>
+                </Sider>
+                <Layout className="site-layout">
+                    <Header className="site-layout-background" style={{ padding: 0 }}> 
+                    <Title style={{ textAlign: 'center' , color: 'blue'}} > Hi {username} you are a {role}</Title>
+                    </Header> 
+                    <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                        <div className="site-layout-background" style={{ padding: 20 }}>
+
+                            <MapPage />
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>Pick-A-Park Created by ByPassaRe </Footer>
+                </Layout>
+            </Layout>
+        </>
     )
 
 }
