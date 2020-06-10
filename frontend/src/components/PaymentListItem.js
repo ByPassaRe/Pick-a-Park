ab1bcc46f2e7ec7bf1860ebf924eb3d88b43997e
Margherita <margherita.renieri@studenti.unicam.it>
2020-06-10 22:50:31 +0200

    Change Payment List Page

--- frontend/src/components/PaymentListItem.js
+++ frontend/src/components/PaymentListItem.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
+import {  Form } from 'antd';
+import '../App.css';
 
 const OutsetDiv = styled.div`
-    border-style: outset;
-    margin: 20px;
+border-style: outset;
+margin: 45px;
+box-shadow: 10px 10px 5px #dce8ea;
 `;
 
 const PaymentListItem = (props) => {
 
     return (
         <OutsetDiv>
-            <p>Id: {props.transaction._id}</p>
-            <p>User ID: {props.transaction.userId}</p>
-            <p>Amount: {props.transaction.amount}</p>
+            <div className="form-container">
+                <Form
+                    name="paymentList"
+                    className="payment-form">
+                    <Form.Item
+                        label="Id:"
+                        name="id"
+                    ><strong>{props.transaction._id}</strong>
+                    </Form.Item>
+                    <Form.Item
+                        label="User ID:"
+                        name="user id"
+                    ><strong> {props.transaction.userId}</strong>
+                    </Form.Item>
+                    <Form.Item
+                        label="Amount:"
+                        name="amount"
+                    ><strong>{props.transaction.amount}</strong>
+                    </Form.Item>
+                </Form>
+                </div>
         </OutsetDiv>
-    );
-}
-
+            );
+        }
+        
 export default PaymentListItem;
